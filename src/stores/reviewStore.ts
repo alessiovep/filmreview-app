2eb5a2a5febe03f48b099fd3ea48c3f07b49f76a
Mariana <dinizmarianaad@gmail.com>
2025-03-26 08:09:52 +0100

    fixed the addReview bug

--- src/stores/reviewStore.ts
+++ src/stores/reviewStore.ts
@@ -1,25 +1,22 @@
-import { defineStore } from 'pinia'
-import axios from 'axios'
+import { defineStore } from "pinia";
+import axios from "axios";
 
-export const useReviewStore = defineStore('reviewStore', {
+export const useReviewStore = defineStore("reviewStore", {
     state: () => ({
-        reviews: [] as any[]
+        reviews: [] as any[],
     }),
     actions: {
         async fetchReviews(filmId?: number) {
-            const baseUrl = 'http://localhost:3001/reviews?_expand=user&_expand=film';
+            const baseUrl = "http://localhost:3001/reviews?_expand=user&_expand=film";
 
-            const url = filmId !== undefined || null
-                ? `${baseUrl}&filmId=${filmId}`
-                : baseUrl;
+            const url = filmId !== undefined || null ? `${baseUrl}&filmId=${filmId}` : baseUrl;
 
             const { data } = await axios.get(url);
             this.reviews = data;
-        }
-        ,
+        },
         async addReview(review: any) {
-            const { data } = await axios.post('http://localhost:3001/reviews', review)
-            this.reviews.push(data)
-        }
-    }
-})
+            const { data } = await axios.post("http://localhost:3001/reviews", review);
+            await this.reviews.push(data);
+        },
+    },
+});
--- src/views/ReviewsView.vue
+++ src/views/ReviewsView.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-container class="mt-6">
-    <h1 class="text-h4 font-weight-bold mb-6">All Reviews</h1>
+    <v-container class="mt-6">
+        <h1 class="text-h4 font-weight-bold mb-6">All Reviews</h1>
 
-    <ReviewList />
-  </v-container>
+        <ReviewList />
+    </v-container>
 </template>
 
 <script setup lang="ts">
 import ReviewList from "../components/ReviewList.vue";
-
 </script>
